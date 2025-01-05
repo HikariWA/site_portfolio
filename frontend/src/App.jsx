f0765de66f500d63a052b175dc6aebca73fc3d37
KASIANGK <ngk.kasia@gmail.com>
2025-01-05 18:00:13 +0100

    suppression Camera car Orbit, zoom/dezzom - ok, manipulation de l'espace autour du mesh - ok

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei'; 
 import Model from './Components/Model';
-import Camera from './Components/Camera';
+import './App.css';
 
 const App = () => {
+  // pour la position de la mouse
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  // calcul du mouvement de la mouse et maj de la position de la mouse
+  const handleMouseMove = (event) => {
+    setMousePosition({
+      x: (event.clientX / window.innerWidth) * 2 - 1,  
+      y: -(event.clientY / window.innerHeight) * 2 + 1, 
+    })
+  }
+
+  // ecouteur pour le mouve de la mouse
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    }
+  }, [])
+
   return (
-    <Canvas className='canvas-custom'>
-      <ambientLight />
-      <spotLight position={[10, 10, 10]} />
-      <Model />
+    <Canvas className='custom-canvas'>
+
+      {/* lumieres */}
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} intensity={0.7} />
+
+      {/* model 3D */}
+      <Model mousePosition={mousePosition} />
+
+      {/* permet a l'user de tourner autour du mesh */}
       <OrbitControls />
-      <Camera></Camera>
+
     </Canvas>
   );
 };
 
 export default App;
+
--- frontend/src/Components/Model.jsx
+++ frontend/src/Components/Model.jsx
@@ -1,10 +1,21 @@
+import React, { useRef, useEffect } from 'react';
 import { useLoader } from '@react-three/fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const Model = () => {
-  const gltf = useLoader(GLTFLoader, '/buildings.glb');
-  console.log(gltf)
-  return <primitive object={gltf.scene} />;
+const Model = ({ mousePosition }) => {
+  const meshRef = useRef();
+  const gltf = useLoader(GLTFLoader, '/buildings.glb');  
+  console.log("let's go voir notre mesh: " + gltf);
+
+  // pour la rotation en fonction de la position de la mouse
+  useEffect(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y = mousePosition.x * Math.PI; // rotation sur axe Y
+      meshRef.current.rotation.x = mousePosition.y * Math.PI / 4; // rotation sur axe X
+    }
+  }, [mousePosition]);
+
+  return <primitive object={gltf.scene} ref={meshRef} scale={[1, 1, 1]} position={[0, 0, 0]} />;
 };
 
 export default Model;
