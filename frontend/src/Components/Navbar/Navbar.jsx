8dff9b57d81eaf8cea05afc24601ea24cbe2461f
KASIANGK <ngk.kasia@gmail.com>
2025-01-09 11:07:58 +0100

    dark/light mode?

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                 <li><a href="/">Home</a></li>
                 <li><a href="/expertise">Expertise</a></li>
                 <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/contact">Ctc</a></li>
                 <li><a href="/members">Members</a></li>
                 <li><a href="/portfolio">Portfolio</a></li>
             </ul>
