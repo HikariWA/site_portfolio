a9beb17509a5b5f2e928294884ceb292b9831d20
KASIANGK <ngk.kasia@gmail.com>
2025-01-09 20:51:43 +0100

    Pop up affichage

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei'; 
 import Model from './Model/Model';
@@ -8,9 +8,10 @@ import Navbar from '../Navbar/Navbar';
 
 const HomePage = () => {
   // pour la position de la mouse
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 })
   const controlsRef = useRef()
-  const [popUp, setPopUp] = useState(0)
+  const [popupIndex, setPopupIndex] = useState(null)
+  const [popupKey, setPopupKey] = useState(0)
   const popups = [
     {
       message: "Hello! Naviguez avec le modèle 3D pour explorer.",
@@ -24,83 +25,102 @@ const HomePage = () => {
       message: "Bonne visite et amusez-vous bien!",
       position: { top: '80%', left: '80%' }
     }
-  ]
+  ];
 
-  // verif si popups ont deja ete vus
-  useEffect(() => {
-    const seenPopups = localStorage.getItem('seenPopups')
-    // console.log("localStorage pour seenPopups : ", seenPopups);
-    if (seenPopups === 'true') {
-      setPopUp(null)
+  const checkPopups = useCallback(() => {
+    let seenPopups = JSON.parse(localStorage.getItem('seenPopups'));
+  
+    // si seenPopups = null ou undefined => tableau de popups non vus
+    if (!seenPopups) {
+      seenPopups = new Array(popups.length).fill(false);
+      localStorage.setItem('seenPopups', JSON.stringify(seenPopups));
     }
-  }, []);
-
-
-  // fonction pour afficher popups
-  const handlePopUp = () => {
-    if (popUp < popups.length - 1) {
-      setPopUp(popUp + 1)
+  
+    // trouver popup non vu
+    const nextPopupIndex = seenPopups.findIndex(seen => !seen);
+  
+  
+    console.log('seenPopups :', seenPopups);
+    console.log('nextPopupIndex :', nextPopupIndex);
+  
+    if (nextPopupIndex === -1) {
+      setPopupIndex(null) // tous les popups vus
+      console.log('Tous les popups ont été vus');
+    } else {
+      setPopupIndex(nextPopupIndex) 
+      console.log('popup avec index :', nextPopupIndex);
+    }
+  }, [popups.length]);
+  
+  const handleNext = () => {
+    if (popupIndex !== null && popupIndex < popups.length - 1) {
+      let seenPopups = JSON.parse(localStorage.getItem('seenPopups'));
+      seenPopups[popupIndex] = true; // pour preciser que popup actuel etait vu
+      localStorage.setItem('seenPopups', JSON.stringify(seenPopups));
+  
+      console.log("popup precise comme vu:" + {popupIndex});
+  
+      setPopupKey(prevKey => prevKey + 1); // forcer le re-rendu
+      checkPopups() // verif popup suoivant
     }
   }
+  
+  const handleLetsGo = () => {
+    let seenPopups = JSON.parse(localStorage.getItem('seenPopups'));
+    seenPopups.fill(true); // mentionne tous les popups comme vus
+    localStorage.setItem('seenPopups', JSON.stringify(seenPopups));
 
-  // si c'est le dernier popup, fermer apres 2sec + enregistrement localStorage si pop-up vus
-  useEffect(() => {
-    if (popUp === popups.length - 1) {
-      const timeout = setTimeout(() => {
-        setPopUp(null)
-        localStorage.setItem('seenPopups', 'true');
-        console.log("popups dans localStorage brrrr")
-      }, 2000)
-      return () => clearTimeout(timeout); // nettoyer le timeout
-    }
-  }, [popUp]);
+    console.log('tous les popups etaient vus');
+  
+    setPopupKey(prevKey => prevKey + 1); 
+    checkPopups() 
+  }
+  
 
   // calcul du mouvement de la mouse et maj de la position de la mouse
   const handleMouseMove = (event) => {
     setMousePosition({
       x: (event.clientX / window.innerWidth) * 2 - 1,  
       y: -(event.clientY / window.innerHeight) * 2 + 1, 
-    })
+    });
   }
 
-  // ecouteur pour le mouve de la mouse
+  // ecouteur pour le mouvement de la mouse
   useEffect(() => {
     window.addEventListener('mousemove', handleMouseMove);
     return () => {
       window.removeEventListener('mousemove', handleMouseMove);
-    }
-  }, [])
-
+    };
+  }, []);
 
   return (
     <div className='homepage-container'>
         <Navbar/>
         {/* <AnimatedText /> */}
-        {popUp !== null && popups[popUp] && (
-          <div
-            className='popup'
-            style={popups[popUp].position}
-          >
+
+        {popupIndex !== null && popups[popupIndex] && (
+          <div className='popup' style={popups[popupIndex].position}>
             <div className='popup-content'>
-              <p>{popups[popUp].message}</p>
-              {popUp < popups.length - 1 ? (
-                <button onClick={handlePopUp}>Next</button>
+              <p>{popups[popupIndex].message}</p>
+              {popupIndex < popups.length - 1 ? (
+                <button onClick={handleNext}>Next</button>
               ) : (
-                <p>Merci et bonne visite !</p>
+                <button onClick={handleLetsGo}>Lets gooo!</button>
               )}
             </div>
           </div>
         )}
+
         <Canvas className='custom-canvas' onClick={(event) => console.log("event position: " + event)}>
 
         {/* lumieres */}
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} intensity={0.7} />
 
-        {/* model 3D */}
+        {/* modele 3D */}
         <Model mousePosition={mousePosition} />
 
-        {/* permet a l'user de tourner autour du mesh */}
+        {/* permet a user de tourner autour du modele */}
         <OrbitControls
             ref={controlsRef}
             maxDistance={10}
@@ -115,8 +135,3 @@ const HomePage = () => {
 };
 
 export default HomePage
-
-
-
-
-
